459251b4810b3ec422b371e7598d80dc63f3992b
Ahmed Younis <ahmedsalah122331@gmail.com>
2025-07-30 20:34:25 +0300

    Center `CircularProgressIndicator` in `ProgressView`

--- app/src/main/java/com/salah/falcon/core/designsystem/compose/ProgressView.kt
+++ app/src/main/java/com/salah/falcon/core/designsystem/compose/ProgressView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,7 +15,8 @@ fun ProgressView(
     modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
         CircularProgressIndicator(
             modifier = modifier.testTag("ProgressView")
